a3adf50ac3616ea03d942d0777b284cab07af46c
SantanuDas2345 <90392244+SantanuDas2345@users.noreply.github.com>
2023-01-10 11:49:11 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,19 @@
 
 
 // export class using module.exports
+var Shape = require('./shape'); 
+  
+ // declare class 
+ class Circle extends Shape{ 
+     constructor(name){ 
+         super(name); 
+  
+     } 
+  
+     calculateArea(radius){ 
+         return Math.PI * Math.pow(radius,2); 
+     } 
+      
+ } 
+  
+ module.exports=Circle; 
